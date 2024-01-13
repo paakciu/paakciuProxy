a6670e3ca35459be860f3aed3abf8d158b9af688
paakciu <asd649433749@qq.com>
2024-01-13 14:46:52 +0800

    P2P准备

---
+++ proxy-core/src/main/java/top/paakciu/proxy/core/protocal/nat/NatChecker.java
@@ -0,0 +1,13 @@
+package top.paakciu.proxy.core.protocal.nat;
+
+/**
+ * @Classname NatChecker
+ * @Date 2024/1/13 14:46
+ * @Created by paakciu
+ */
+public class NatChecker {
+
+    public static void main(String[] args) {
+
+    }
+}
